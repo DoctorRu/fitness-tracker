ae7f96cf27d8f9dd455426cfbdb49253437b2ef8
DoctorRu <vjblan@gmail.com>
2018-03-21 00:33:16 -0300

    DataTable sorting

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {MatTableDataSource} from '@angular/material';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {MatSort, MatTableDataSource} from '@angular/material';
 
 import {Exercise} from '../exercise.model';
 import {TrainingService} from '../training.service';
@@ -9,10 +9,12 @@ import {TrainingService} from '../training.service';
     templateUrl: './past-training.component.html',
     styleUrls: [ './past-training.component.css' ]
 })
-export class PastTrainingComponent implements OnInit {
+
+export class PastTrainingComponent implements OnInit, AfterViewInit {
     displayedColumns = [ 'date', 'name', 'calories', 'duration', 'state' ];
     dataSource = new MatTableDataSource<Exercise>();
     
+    @ViewChild(MatSort) sort: MatSort;
     
     constructor(private trainingService: TrainingService) {
     }
@@ -20,5 +22,10 @@ export class PastTrainingComponent implements OnInit {
     ngOnInit() {
         this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
     }
+    
+    ngAfterViewInit() {
+        this.dataSource.sort = this.sort;
+    }
+    
     
 }
