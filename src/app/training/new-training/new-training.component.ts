a772577ff76b8d5cd1bd9d7133757b10576b8850
DoctorRu <vjblan@gmail.com>
2018-03-22 01:30:50 -0300

    Firebase

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {TrainingService} from '../training.service';
 import {NgForm} from '@angular/forms';
-import {AngularFirestore} from 'angularfire2/firestore';
-import {Observable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
+
+import {Exercise} from '../exercise.model';
+
 
 @Component({
     selector: 'app-new-training',
@@ -10,20 +12,29 @@ import {Observable} from 'rxjs/Observable';
     styleUrls: [ './new-training.component.css' ]
 })
 
-export class NewTrainingComponent implements OnInit {
-    exercises: Observable<any>;
+export class NewTrainingComponent implements OnInit, OnDestroy {
+    exercises: Exercise[];
+    exerciseSubscription: Subscription;
     
-    constructor(private trainingService: TrainingService,
-                private db: AngularFirestore) {
+    constructor(private trainingService: TrainingService) {
+    
     }
     
     ngOnInit() {
-        this.exercises = this.db.collection('availableExercises')
-            .valueChanges();
+        this.exerciseSubscription =  this.trainingService.exercisesChanged.subscribe(
+            exercises => {
+                this.exercises = exercises;
+            }
+        );
+        this.trainingService.fetchAvailableExercises();
     }
     
     onStartTraining(form: NgForm) {
         this.trainingService.startExercise(form.value.exercise);
+    }
+    
+    ngOnDestroy() {
+        this.exerciseSubscription.unsubscribe();
     }
     
 }
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,24 +1,41 @@
+import {Injectable} from '@angular/core';
 import {Subject} from 'rxjs/Subject';
+import {AngularFirestore} from 'angularfire2/firestore';
 
 import {Exercise} from './exercise.model';
 
+@Injectable()
 export class TrainingService {
     
     exerciseChanged = new Subject<Exercise>();
+    exercisesChanged = new Subject<Exercise[]>();
     
-    private availableExercises: Exercise[] = [
-        {id: 'crunches', name: 'Crunches', duration: 30, calories: 8},
-        {id: 'touch-toes', name: 'Touches Toes', duration: 180, calories: 15},
-        {id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18},
-        {id: 'nurpees', name: 'Burpees', duration: 60, calories: 8}
-    ];
+    private availableExercises: Exercise[] = [];
     
     private runningExercise: Exercise;
     private exercises: Exercise[] = [];
     
+    constructor(private db: AngularFirestore) {
     
-    getAvailableExercises() {
-        return this.availableExercises.slice();
+    }
+    
+    fetchAvailableExercises() {
+        this.db.collection('availableExercises')
+            .snapshotChanges()
+            .map(docArray => {
+                return docArray.map(doc => {
+                    return {
+                        id: doc.payload.doc.id,
+                        name: doc.payload.doc.data().name,
+                        duration: doc.payload.doc.data().duration,
+                        calories: doc.payload.doc.data().calories,
+                    };
+                });
+            })
+            .subscribe((exercises: Exercise[]) => {
+                this.availableExercises = exercises;
+                this.exercisesChanged.next([...this.availableExercises]);
+            });
     }
     
     startExercise(selectedId: string) {
@@ -30,7 +47,7 @@ export class TrainingService {
     }
     
     completeExercise() {
-        this.exercises.push(
+        this.addDataToDatabase(
             {
                 ...this.runningExercise,
                 date: new Date(),
@@ -42,7 +59,7 @@ export class TrainingService {
     }
     
     cancelExercise(progress: number) {
-        this.exercises.push(
+        this.addDataToDatabase(
             {
                 ...this.runningExercise,
                 duration: this.runningExercise.duration * (progress / 100),
@@ -63,5 +80,9 @@ export class TrainingService {
         return this.exercises.slice();
     }
     
+    private addDataToDatabase(exercise: Exercise) {
+        this.db.collection('finishedExercises').add(exercise);
+    
+    }
 }
 
